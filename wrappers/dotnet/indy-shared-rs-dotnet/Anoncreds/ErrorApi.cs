a147ba21c681c31a01aeade0161b725aa3f47cf9
Jan Wameling <j.wameling@esatus.com>
2022-11-28 12:26:33 +0100

    copied wrapper from indy_shared_rs and changed nativemethods

---
+++ wrappers/dotnet/indy-shared-rs-dotnet/Anoncreds/ErrorApi.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace indy_shared_rs_dotnet.Anoncreds
+{
+    public static class ErrorApi
+    {
+        /// <summary>
+        /// Returns json <see cref="string"/> of the last thrown native error.
+        /// </summary>
+        /// <returns>Error json in form of <c>{"code":[int],"message":[string]}</c>.</returns>
+        public static Task<string> GetCurrentErrorAsync()
+        {
+            string result = "";
+            _ = NativeMethods.anoncreds_get_current_error(ref result);
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ wrappers/dotnet/indy-shared-rs-dotnet/Anoncreds/ModApi.cs
@@ -0,0 +1,32 @@
+﻿using indy_shared_rs_dotnet.Models;
+using System.Threading.Tasks;
+
+namespace indy_shared_rs_dotnet.Anoncreds
+{
+    public static class ModApi
+    {
+        /// <summary>
+        /// Sets the default logger.
+        /// </summary>
+        /// <exception cref="SharedRsException">Throws when logger can not be set.</exception>
+        public static async Task SetDefaultLoggerAsync()
+        {
+            int errorCode = NativeMethods.anoncreds_set_default_logger();
+            if (errorCode != 0)
+            {
+                string error = await ErrorApi.GetCurrentErrorAsync();
+                throw SharedRsException.FromSdkError(error);
+            }
+        }
+
+        /// <summary>
+        /// Gets the current library version.
+        /// </summary>
+        /// <returns>Version number as <see cref="string"/> representation.</returns>
+        public static Task<string> GetVersionAsync()
+        {
+            string result = NativeMethods.anoncreds_version();
+            return Task.FromResult(result);
+        }
+    }
+}
