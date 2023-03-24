31914254a140ed1b0b12ae4bbeff13ece95904ac
pstenger <p.stenger@esatus.com>
2023-03-24 13:47:59 +0100

    added JsonProperty

--- wrappers/dotnet/anoncreds-rs-dotnet/Models/KeyProofAttributeValue.cs
+++ wrappers/dotnet/anoncreds-rs-dotnet/Models/KeyProofAttributeValue.cs
@@ -1,8 +1,12 @@
-﻿namespace anoncreds_rs_dotnet.Models
+﻿using Newtonsoft.Json;
+
+namespace anoncreds_rs_dotnet.Models
 {
     public class KeyProofAttributeValue
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("value")]
         public string Value { get; set; }
 
         public KeyProofAttributeValue(string name, string value)
